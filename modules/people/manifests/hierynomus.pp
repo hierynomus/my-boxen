813519df2bebcf1a0b753f376663853d0885863c
hierynomus <jeroen@javadude.nl>
2014-10-30 11:03:43 +0100

    Personalizing...

---
+++ modules/people/manifests/hierynomus.pp
@@ -0,0 +1,23 @@
+class people::hierynomus {
+  include onepassword
+  include evernote
+  include sonos
+  include dropbox
+  include iterm2::dev
+  include iterm2::colors::solarized_dark
+  include iterm2::colors::solarized_light
+
+
+  # $home     = "/Users/${::boxen_user}"
+  # $my       = "${home}/my"
+  # $dotfiles = "${my}/dotfiles"
+  
+  # file { $my:
+  #   ensure  => directory
+  # }
+
+  # repository { $dotfiles:
+  #   source  => 'jbarnette/dotfiles',
+  #   require => File[$my]
+  # }
+}
